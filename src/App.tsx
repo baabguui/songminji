da5efcf028b33549c7eec8005aee48490a499736
임지원 <9universe9th@gmail.com>
2023-11-28 18:45:05 +0900

    fix : build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ROUTES_LIST } from "./routes";
+import Home from "./pages/Home";
 import "./css/app.css";
 
 function App() {
     return (
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <Routes>
+                <Route path="/" element={<Home />} />
                 {ROUTES_LIST.map(({ path, Component }, idx) => (
                     <Route key={idx} path={path} element={<Component />} />
                 ))}
