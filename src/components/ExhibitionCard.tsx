0a78f5f8300e607d0527b54c2210e16d467ab0d8
임지원 <9universe9th@gmail.com>
2024-01-18 16:55:11 +0700

    chore: 이미지 이름 수정

--- src/components/ExhibitionCard.tsx
+++ src/components/ExhibitionCard.tsx
@@ -18,7 +18,10 @@ const ExhibitionCard = (props: {
         <img
           className="poster"
           src={
-            "/assets/2023/exhibitions/" + props.exhibition.title + "/1" + ".jpg"
+            "/assets/2023/exhibitions/" +
+            props.exhibition.title +
+            "/1_preview" +
+            ".jpg"
           }
           alt="poster"
         />
