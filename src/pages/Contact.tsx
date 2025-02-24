dac0ce6017d8e9f889b4e517737ce4f784f6246d
baabguui <9universe9th@gmail.com>
2025-02-25 02:19:37 +0900

    chore: fontStyle -> fontSize

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,7 +3,7 @@ import { ContactContainer } from "styles/ContactStyles";
 const Contact = () => {
   return (
     <ContactContainer>
-      <p style={{ fontStyle: "clamp(10px, 1.2vw, 22px)", margin: "0" }}>
+      <p style={{ fontSize: "clamp(10px, 1.2vw, 22px)", margin: "0" }}>
         songminji1918@gmail.com
       </p>
     </ContactContainer>
