862272cc67f646b2ce09a3b329624c92bad9e457
baabguui <9universe9th@gmail.com>
2025-02-26 01:43:59 +0900

    styles: CV (모바일)디자인,

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -1,116 +1,160 @@
 import {
+  Container,
   CVContainer,
-  CVCategoryContainer,
+  CVLanguageContainer,
+  CVYearContainer,
   CVContentContainer,
   CVContentParagraph,
 } from "styles/CVStyles";
 
 const CV = () => {
   return (
-    <CVContainer>
-      <CVContentParagraph
-        style={{ fontSize: "clamp(10px, 1.2vw, 22px)", paddingBottom: "1.8vw" }}
-      >
+    <Container>
+      <CVContentParagraph style={{ paddingBottom: "1.8vw" }}>
         송민지 Minji Song
       </CVContentParagraph>
-      {cv.map((category, index) => {
-        return (
-          <CVCategoryContainer key={index}>
-            {category.map((content) => {
-              return (
-                <CVContentContainer key={content.year}>
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      width: "29vw",
-                      gap: "1vw",
-                    }}
-                  >
-                    <CVContentParagraph>{content.year}</CVContentParagraph>
-                    <div>
-                      {content.kr.map((p) => {
-                        return (
-                          <CVContentParagraph key={p}>{p}</CVContentParagraph>
-                        );
-                      })}
-                    </div>
-                  </div>
-                  <div
-                    style={{
-                      display: "flex",
-                      flexDirection: "row",
-                      width: "32vw",
-                      gap: "1vw",
-                    }}
-                  >
-                    <CVContentParagraph>{content.year}</CVContentParagraph>
-                    <div>
-                      {content.en.map((p) => {
-                        return (
-                          <CVContentParagraph key={p}>{p}</CVContentParagraph>
-                        );
-                      })}
-                    </div>
-                  </div>
-                </CVContentContainer>
-              );
-            })}
-          </CVCategoryContainer>
-        );
-      })}
-    </CVContainer>
+      <CVContainer>
+        {cv.map((lan, index) => {
+          return (
+            <CVLanguageContainer key={index}>
+              {Object.entries(lan[0]).map(([category, categoryContents]) => {
+                return (
+                  <>
+                    {categoryContents.map((year, index) => {
+                      return (
+                        <CVContentContainer key={index}>
+                          {Object.entries(year)
+                            .sort(
+                              ([yearA], [yearB]) =>
+                                Number(yearB) - Number(yearA),
+                            )
+                            .map((content, index) => {
+                              return (
+                                <CVYearContainer
+                                  key={index}
+                                  category={category}
+                                >
+                                  <CVContentParagraph>
+                                    {content[0]}
+                                  </CVContentParagraph>
+                                  <CVContentContainer>
+                                    {content[1].map((item, index) => {
+                                      return (
+                                        <CVContentParagraph key={index}>
+                                          {item}
+                                        </CVContentParagraph>
+                                      );
+                                    })}
+                                  </CVContentContainer>
+                                </CVYearContainer>
+                              );
+                            })}
+                        </CVContentContainer>
+                      );
+                    })}
+                  </>
+                );
+              })}
+            </CVLanguageContainer>
+          );
+        })}
+      </CVContainer>
+    </Container>
   );
 };
 
 export default CV;
 
+const korean = [
+  {
+    educations: [{ 2022: ["서울과학기술대학교 조형예술과 학사 졸업"] }],
+    exhibitions: [
+      {
+        2025: ["공실, 서울시 강남구 압구정로 75길 38-17 2층"],
+        2024: [
+          "겨울 회화, 대안 공간 루프",
+          "파크, 상히읗",
+          "언두 이펙트, 하이트 컬렉션",
+          "웰, 인터럼",
+        ],
+        2023: ["포 니들스, 에브리아트", "퀘스트, 갤러리 인"],
+        2022: ["피어 투 피어, 온수 공간"],
+        2020: ["미리 찌는 살, 웨스"],
+      },
+    ],
+  },
+];
+
+const english = [
+  {
+    educations: [
+      {
+        2022: [
+          "BFA Fine Arts, Seoul National University of Science & Technology, Seoul",
+        ],
+      },
+    ],
+    exhibitions: [
+      {
+        2025: ["Vacancy, Apgujeong-ro 75-gil, Seoul"],
+        2024: [
+          "Painting in Winter, Alternative Space Loop",
+          "Park, sangheeut",
+          "Undo Effects, Hite Collection",
+          "WELL, INTERIM",
+        ],
+        2023: ["4 Needles, every Art", "QUEST, gallery IN"],
+        2022: ["Peer to Peer, onsu gonggan"],
+        2020: ["Fatten up for Tomorrow, WESS"],
+      },
+    ],
+  },
+];
+
 const educations = [
   {
-    year: 2022,
+    kr: [{ 2022: ["서울과학기술대학교 조형예술과 학사 졸업"] }],
     en: [
-      "BFA Fine Arts, Seoul National University of Science & Technology, Seoul",
+      {
+        2022: [
+          "BFA Fine Arts, Seoul National University of Science & Technology, Seoul",
+        ],
+      },
     ],
-    kr: ["서울과학기술대학교 조형예술과 학사 졸업"],
   },
 ];
 
 const exhibitions = [
   {
-    year: 2025,
-    en: ["Vacancy, Apgujeong-ro 75-gil, Seoul"],
-    kr: ["공실, 서울시 강남구 압구정로 75길 38-17 2층"],
-  },
-  {
-    year: 2024,
-    en: [
-      "Painting in Winter, Alternative Space Loop",
-      "Park, sangheeut",
-      "Undo Effects, Hite Collection",
-      "WELL, INTERIM",
-    ],
     kr: [
-      "겨울 회화, 대안 공간 루프",
-      "파크, 상히읗",
-      "언두 이펙트, 하이트 컬렉션",
-      "웰, 인터럼",
+      {
+        2025: ["공실, 서울시 강남구 압구정로 75길 38-17 2층"],
+        2024: [
+          "겨울 회화, 대안 공간 루프",
+          "파크, 상히읗",
+          "언두 이펙트, 하이트 컬렉션",
+          "웰, 인터럼",
+        ],
+        2023: ["포 니들스, 에브리아트", "퀘스트, 갤러리 인"],
+        2022: ["피어 투 피어, 온수 공간"],
+        2020: ["미리 찌는 살, 웨스"],
+      },
     ],
-  },
-  {
-    year: 2023,
-    en: ["4 Needles, every Art", "QUEST, gallery IN"],
-    kr: ["포 니들스, 에브리아트", "퀘스트, 갤러리 인"],
-  },
-  {
-    year: 2022,
-    en: ["Peer to Peer, onsu gonggan"],
-    kr: ["피어 투 피어, 온수 공간"],
-  },
-  {
-    year: 2020,
-    en: ["Fatten up for Tomorrow, WESS"],
-    kr: ["미리 찌는 살, 웨스"],
+    en: [
+      {
+        2025: ["Vacancy, Apgujeong-ro 75-gil, Seoul"],
+        2024: [
+          "Painting in Winter, Alternative Space Loop",
+          "Park, sangheeut",
+          "Undo Effects, Hite Collection",
+          "WELL, INTERIM",
+        ],
+        2023: ["4 Needles, every Art", "QUEST, gallery IN"],
+        2022: ["Peer to Peer, onsu gonggan"],
+        2020: ["Fatten up for Tomorrow, WESS"],
+      },
+    ],
   },
 ];
 
-export const cv = [educations, exhibitions];
+export const cv = [korean, english];
--- src/styles/CVStyles.ts
+++ src/styles/CVStyles.ts
@@ -1,32 +1,70 @@
 import styled from "styled-components";
 
-const CVContainer = styled.div`
+const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 70vw;
-  gap: 1.5vw;
+  @media (max-width: 768px) {
+    gap: 6vw;
+  }
 `;
 
-const CVCategoryContainer = styled.div`
+const CVContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 70vw;
+  gap: 0vw;
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 6vw;
+  }
+`;
+
+const CVLanguageContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 0.1vw;
+  gap: 1vw;
+  @media (max-width: 768px) {
+    gap: 6vw;
+  }
+`;
+
+interface YearContainerProps {
+  //kr, en 간격 맞추기용
+  category: string;
+}
+
+const CVYearContainer = styled.div<YearContainerProps>`
+  display: flex;
+  flex-direction: row;
+  width: 30vw;
+  gap: 1vw;
+  min-height: ${({ category }) => (category === "educations" ? "4vw" : 0)};
+  @media (max-width: 768px) {
+    width: 80vw;
+  }
 `;
 
 const CVContentContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
 `;
 
 const CVContentParagraph = styled.p`
   font-size: clamp(10px, 1.2vw, 22px);
   font-family: Malgun Gothic;
   margin: -0.1vw;
+
+  @media (max-width: 768px) {
+    font-size: clamp(12px, 3.4vw, 24px);
+    margin: 0;
+  }
 `;
 
 export {
+  Container,
   CVContainer,
-  CVCategoryContainer,
-  CVContentContainer,
+  CVLanguageContainer,
+  CVYearContainer,
   CVContentParagraph,
+  CVContentContainer,
 };
