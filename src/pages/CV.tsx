0463bb4655dbcd1495d1950d55511eb6fa2fdc80
baabguui <9universe9th@gmail.com>
2025-02-26 23:58:33 +0900

    style: cv 카테고리 구분

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -8,6 +8,24 @@ import {
 } from "styles/CVStyles";
 
 const CV = () => {
+  const renderCategory = (language: string, category: string) => {
+    const descriptions: Record<string, Record<string, string>> = {
+      korean: {
+        soloExhibitions: "개인전",
+        groupExhibitions: "단체전",
+      },
+      english: {
+        soloExhibitions: "Solo Exhibitions",
+        groupExhibitions: "Group Exhibitions",
+      },
+    };
+    console.log(language);
+
+    const matchedDescription = descriptions[language]?.[category];
+    if (matchedDescription) {
+      return <CVContentParagraph>{matchedDescription}</CVContentParagraph>;
+    }
+  };
   return (
     <Container>
       <CVContentParagraph style={{ paddingBottom: "1.8vw" }}>
@@ -15,11 +33,13 @@ const CV = () => {
       </CVContentParagraph>
       <CVContainer>
         {cv.map((lan, index) => {
+          const lanName = index === 0 ? "korean" : "english";
           return (
             <CVLanguageContainer key={index}>
               {Object.entries(lan[0]).map(([category, categoryContents]) => {
                 return (
                   <>
+                    {renderCategory(lanName, category)}
                     {categoryContents.map((year, index) => {
                       return (
                         <CVContentContainer key={index}>
@@ -68,15 +88,15 @@ export default CV;
 const korean = [
   {
     educations: [{ 2022: ["서울과학기술대학교 조형예술과 학사 졸업"] }],
-    exhibitions: [
+    soloExhibitions: [
+      {
+        2024: ["파크, 상히읗", "웰, 인터럼"],
+      },
+    ],
+    groupExhibitions: [
       {
         2025: ["공실, 서울시 강남구 압구정로 75길 38-17 2층"],
-        2024: [
-          "겨울 회화, 대안 공간 루프",
-          "파크, 상히읗",
-          "언두 이펙트, 하이트 컬렉션",
-          "웰, 인터럼",
-        ],
+        2024: ["겨울 회화, 대안 공간 루프", "언두 이펙트, 하이트 컬렉션"],
         2023: ["포 니들스, 에브리아트", "퀘스트, 갤러리 인"],
         2022: ["피어 투 피어, 온수 공간"],
         2020: ["미리 찌는 살, 웨스"],
@@ -94,14 +114,17 @@ const english = [
         ],
       },
     ],
-    exhibitions: [
+    soloExhibitions: [
+      {
+        2024: ["Park, sangheeut", "WELL, INTERIM"],
+      },
+    ],
+    groupExhibitions: [
       {
         2025: ["Vacancy, Apgujeong-ro 75-gil, Seoul"],
         2024: [
           "Painting in Winter, Alternative Space Loop",
-          "Park, sangheeut",
           "Undo Effects, Hite Collection",
-          "WELL, INTERIM",
         ],
         2023: ["4 Needles, every Art", "QUEST, gallery IN"],
         2022: ["Peer to Peer, onsu gonggan"],
