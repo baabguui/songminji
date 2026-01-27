6b55a19a2eeedb47bee464f3dfdc6fecfa97fda2
baabguui <9universe9th@gmail.com>
2026-01-27 21:36:07 +0900

    assets: OLM Project 추가

--- src/pages/CV.tsx
+++ src/pages/CV.tsx
@@ -94,6 +94,7 @@ const korean = [
     educations: [{ 2022: ["서울과학기술대학교 조형예술과 학사 졸업"] }],
     soloExhibitions: [
       {
+        2025: ["옮 프로젝트"],
         2024: ["파크, 상히읗", "웰, 인터럼"],
       },
     ],
@@ -120,6 +121,7 @@ const english = [
     ],
     soloExhibitions: [
       {
+        2025: ["OLM Project"],
         2024: ["Park, sangheeut", "WELL, INTERIM"],
       },
     ],
